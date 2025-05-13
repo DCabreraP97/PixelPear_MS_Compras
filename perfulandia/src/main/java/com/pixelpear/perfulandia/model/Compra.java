90a1f7a66d7a2814c0b890031a6b77baf19e5c41
Vicente <vic.bravoc@duocuc.cl>
2025-05-13 15:39:09 -0400

    Cree la clase Compra.java para probar la conexion a la base de datos. Modifique el archivo properties. Resultó con exito la conexion a la base de datos

---
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/model/Compra.java
@@ -0,0 +1,23 @@
+package com.pixelpear.perfulandia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productoId;
+    private Integer cantidad;
+    private Double precioUnitario;
+    private Double precioTotal;
+}
