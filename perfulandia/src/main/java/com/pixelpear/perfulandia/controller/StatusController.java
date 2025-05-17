83ffd79216bcb59f6dcdbcc77f98086fb2de600b
Vicente <vic.bravoc@duocuc.cl>
2025-05-17 16:44:57 -0400

    Se quitaron importaciones sin usar

--- perfulandia/src/main/java/com/pixelpear/perfulandia/controller/StatusController.java
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/controller/StatusController.java
@@ -3,7 +3,6 @@ package com.pixelpear.perfulandia.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
