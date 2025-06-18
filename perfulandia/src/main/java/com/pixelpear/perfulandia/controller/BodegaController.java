bcb71d2fd7e820a98ae2b65a0d2171fb5da087b9
Vicente <vic.bravoc@duocuc.cl>
2025-06-18 11:48:18 -0400

    Se agrega BodegaController CRUD de perfumes en BD.

---
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/controller/BodegaController.java
@@ -0,0 +1,75 @@
+package com.pixelpear.perfulandia.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pixelpear.perfulandia.model.Perfume;
+import com.pixelpear.perfulandia.service.PerfumeService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("bodega")
+public class BodegaController {
+    
+    private final PerfumeService perfumeService;
+
+    @GetMapping("/perfumes")
+    public ResponseEntity<List<Perfume>> mostrarPerfumes() {
+        return ResponseEntity.ok(perfumeService.mostrarPerfumes());
+    }
+    
+    @GetMapping("/perfume/{idPerfume}")
+    public ResponseEntity<Perfume> mostrarPerfume(@PathVariable Long idPerfume) {
+        Perfume perfume = perfumeService.buscarPerfumePorId(idPerfume);
+        if (perfume != null) {
+            return ResponseEntity.ok(perfume);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping("/agregarPerfume")
+    public ResponseEntity<Perfume> agregarPerfume(@RequestBody Perfume perfume) {
+        perfumeService.guardarPerfume(perfume);
+        return ResponseEntity.ok(perfume);
+    }
+    
+    @DeleteMapping("/eliminarPerfume/{idPerfume}")
+    public ResponseEntity<String> eliminarPerfume(@PathVariable Long idPerfume) {
+        if (perfumeService.existePerfume(idPerfume)) {
+            perfumeService.eliminarPerfume(idPerfume);
+            return ResponseEntity.ok("Perfume eliminado con éxito");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/actualizarPerfume/{idPerfume}")
+    public ResponseEntity<Perfume> actualizarPerfume(@PathVariable Long idPerfume, @RequestBody Perfume perfume) {
+        if (perfumeService.existePerfume(idPerfume)) {
+            perfume.setIdPerfume(idPerfume);
+            perfumeService.guardarPerfume(perfume);
+            return ResponseEntity.ok(perfume);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
--- perfulandia/src/main/java/com/pixelpear/perfulandia/service/PerfumeService.java
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/service/PerfumeService.java
@@ -1,5 +1,7 @@
 package com.pixelpear.perfulandia.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.pixelpear.perfulandia.model.Perfume;
@@ -12,14 +14,26 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class PerfumeService {
+
     private final PerfumeRepository perfumeRepository;
+
     public boolean existePerfume(Long idPerfume) {
         return perfumeRepository.existsById(idPerfume);
     }
+
     public Perfume buscarPerfumePorId(Long idPerfume) {
         return perfumeRepository.findByIdPerfume(idPerfume);
     }
+
     public void guardarPerfume(Perfume perfumeBD) {
         perfumeRepository.save(perfumeBD);
+    }
+
+    public List<Perfume> mostrarPerfumes() {
+        return perfumeRepository.findAll();
+    }
+
+    public void eliminarPerfume(Long idPerfume) {
+        perfumeRepository.deleteById(idPerfume);
     }
 }
