331d0d2d72c812bb856780445b5c4ab43457ca2d
Vicente <vic.bravoc@duocuc.cl>
2025-06-17 22:22:48 -0400

    Se soluciono error, faltaba entity en Perfume. Falta por solucionar: crear PerfumeDto.

--- perfulandia/src/main/java/com/pixelpear/perfulandia/model/Perfume.java
+++ perfulandia/src/main/java/com/pixelpear/perfulandia/model/Perfume.java
@@ -1,15 +1,22 @@
 package com.pixelpear.perfulandia.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class Perfume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPerfume;
     private String nombre;
     private double precio;
