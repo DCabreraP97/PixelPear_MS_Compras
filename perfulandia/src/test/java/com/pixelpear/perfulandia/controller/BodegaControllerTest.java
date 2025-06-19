911d10c23524c473710a83f40def2d106e80bece
Vicente <vic.bravoc@duocuc.cl>
2025-06-19 17:29:22 -0400

    Se agrega mockito a las dependecies

---
+++ perfulandia/src/test/java/com/pixelpear/perfulandia/controller/BodegaControllerTest.java
@@ -0,0 +1,10 @@
+package com.pixelpear.perfulandia.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class BodegaControllerTest {
+    @Test
+    public void testBodegaController() {
+        
+    }
+}
